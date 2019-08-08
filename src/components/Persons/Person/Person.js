c6bdd732c39ac1abacd320b2e7ea3b12b1ef89a7
waqas <waqas.hassan@nxb.com.pk>
2019-08-08 18:40:03 +0500

    New method of creating Ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,7 @@ class Person extends Component{
     constructor(props){
         super(props);
         console.log("[Person.js] Inside Constructor ", props);
+        this.inputElement = React.createRef();
     }
 
     componentWillMount() {
@@ -17,7 +18,7 @@ class Person extends Component{
         console.log("[Person.js] Component did mount");
         //Mostly for adjusting focus and media playback.
         if(this.props.position===0){
-            this.inputElement.focus();    
+            this.inputElement.current.focus();    
         }
         
     }
@@ -32,7 +33,7 @@ class Person extends Component{
                 <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={ (inp) => {this.inputElement = inp} }
+                    ref={ this.inputElement }
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}/>
